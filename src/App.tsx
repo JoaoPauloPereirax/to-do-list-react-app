f6c16b23cb2671bb4da9da431fc05ea0be8b30b2
João Paulo Pereira <joaopaulop.dev@gmail.com>
2023-06-27 12:48:37 -0300

    Add ts item

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-const App = ()=>{
-  return (
+//PACOTES
+import {useState} from 'react';
+import * as C from './App.styles';
+import {Item} from '../src/types/Item'; 
 
+const App = () => {
+  const[list, setList] = useState<Item[]>([
+    {id:1, name:"Levantar", done:true},
+    {id:1, name:"Ir ao trabalho", done:true}
+  ]);
+
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>To-do List</C.Header>
+        {list.map((item, index)=>(
+          <div>{item.name}</div>
+        ))}
+      </C.Area>
+    </C.Container>
   );
 }
 
