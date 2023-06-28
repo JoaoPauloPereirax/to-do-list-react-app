bae68f475614c53afbe483da11c65346ee26e5f4
João Paulo Pereira <joaopaulop.dev@gmail.com>
2023-06-27 22:17:45 -0300

    Add components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@
 import {useState} from 'react';
 import * as C from './App.styles';
 import {Item} from '../src/types/Item'; 
+import {ListItem} from './components/ListItem';
 
 const App = () => {
   const[list, setList] = useState<Item[]>([
     {id:1, name:"Levantar", done:true},
-    {id:1, name:"Ir ao trabalho", done:true}
+    {id:1, name:"Ir ao trabalho", done:false}
   ]);
 
   return (
@@ -14,7 +15,7 @@ const App = () => {
       <C.Area>
         <C.Header>To-do List</C.Header>
         {list.map((item, index)=>(
-          <div>{item.name}</div>
+          <ListItem key={index} item={item}/>
         ))}
       </C.Area>
     </C.Container>
